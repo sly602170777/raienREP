aab13e603426636c2efce5ac2210186313cb5852
rellayduke <sly602170777@gmail.com>
2020-11-28 15:37:00 +0900

    API

---
+++ Ajax＿jQuery＿API01/Ajax＿jQuery＿API01/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Ajax＿jQuery＿API01.WebApiApplication" Language="C#" %>
---
+++ Ajax＿jQuery＿API01/Ajax＿jQuery＿API01/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Ajax＿jQuery＿API01.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+    }
+}
