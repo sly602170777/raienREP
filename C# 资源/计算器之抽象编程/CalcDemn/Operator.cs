a86b9aad4d84ca286df0164ac546013724e23997
rellayduke <sly602170777@gmail.com>
2021-04-18 22:43:53 +0900

    计算器之抽象编程

---
+++ C# 资源/计算器之抽象编程/CalcDemn/Operator.cs
@@ -0,0 +1,38 @@
+﻿namespace CalculateDemo
+{
+    abstract class Operator
+    {
+        public decimal Left { get; set; }
+        public decimal Right { get; set; }
+        public abstract decimal Calc();
+    }
+        class AddOperator : Operator
+    {
+        public override decimal Calc()
+        {
+            return Left + Right;
+        }
+    }
+    class SubOperator : Operator
+    {
+        public override decimal Calc()
+        {
+            return Left - Right;
+        }
+    }
+    class MulOperator : Operator
+    {
+        public override decimal Calc()
+        {
+            return Left * Right;
+        }
+    }
+    class DivOperrator : Operator
+    {
+        public override decimal Calc()
+        {
+            decimal a= Left / Right;
+            return a;
+        }
+    }
+}
