518220da5a68cf013b5d072bc5f46f7441c7d376
sly <sly6021070777@gmail.com>
2025-02-24 15:52:14 +0900

    winform再修正

--- C# 资源/电子书籍-bookwalker_C#_templt/VCS2019_Sample/Chapter1 Visual C#2019の基礎/1-1 Visual C# 2019の基本 001-024/kiso009/kiso009/Form1.cs
+++ C# 资源/电子书籍-bookwalker_C#_templt/VCS2019_Sample/Chapter1 Visual C#2019の基礎/1-1 Visual C# 2019の基本 001-024/kiso009/kiso009/Form1.cs
@@ -1,25 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace kiso009
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1 ()
         {
-            InitializeComponent();
+            //Windowサイズを変更しないように
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+
+            if (MaximizeBox & MinimizeBox)
+            {
+                MaximizeBox = false;
+                MinimizeBox = false;
+            }
+            else
+            {
+                MaximizeBox = true;
+                MinimizeBox = true;
+            }
+
+            InitializeComponent ();
         }
 
-        private void Button1_Click(object sender, EventArgs e)
+        private void Button1_Click (object sender, EventArgs e)
         {
+
             label1.Text = "Visual C# 2019をはじめよう！";
+            //using linq
+            MessageBox.Show ("这是form2", "这是form2 ShowDialog");
+            Form2 form2 = new Form2 ();
+            //不允许选择其他form
+            form2.ShowDialog ();
+
+        }
+
+        private void Button2_Click (object sender, EventArgs e)
+        {
+            MessageBox.Show ("这是form2", "这是form2 Show");
+            Form2 form2 = new Form2 ();
+            //允许选择其他form
+            form2.Show ();
+        }
+
+        private void Form1_Load (object sender, EventArgs e)
+        {
+            MessageBox.Show ("on load ", " on load 処理");
         }
     }
 }
