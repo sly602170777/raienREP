837ca9adf5644312e58824c233692cc2df7d8964
sly <sly6021070777@gmail.com>
2025-03-01 22:48:26 +0900

    controller+json+ajax -ver.002

---
+++ C# 资源/asp.net/aspnetMVCProject/aspnetMVCProject/Controllers/JsonResultDemoController.cs
@@ -0,0 +1,101 @@
+﻿using aspnetMVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetMVCProject.Controllers
+{
+    public class JsonResultDemoController: Controller
+    {
+        #region ActionControllers
+
+        public JsonResult WelcomeNote ()
+        {
+            bool isAdmin = false;
+            string output = isAdmin ? "Welcome" : "Hello World";
+
+            return Json (output, JsonRequestBehavior.AllowGet);
+        }
+
+
+        private List<user> GetUsers ()
+        {
+            var usersList = new List<user>
+         {
+            new user
+            {
+               Id = "11",
+               Name = "Tapas1",
+              
+            },
+            new user
+            {
+               Id = "12",
+               Name = "Robin2",
+           
+            },
+            new user
+            {
+               Id = "13",
+               Name = "Robin3",
+            
+            }
+         };
+
+            return usersList;
+        }
+
+        public JsonResult GetUsersData ()
+        {
+            var users = GetUsers ();
+            return Json (users, JsonRequestBehavior.AllowGet);
+        }
+
+        private List<user> GetUsersHugeData ()
+        {
+            var usersList = new List<user> ();
+            user user;
+            for (int i = 1; i < 51000; i++)
+            {
+                user = new user
+                {
+                    Id = i +"",
+                    Name = "user-" + i,
+
+                };
+                usersList.Add (user);
+            }
+
+            return usersList;
+        }
+
+        public JsonResult GetUsersHugeList ()
+        {
+            var users = GetUsersHugeData ();
+            return Json (users, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SampleView ()
+        {
+            return View ();
+        }
+        protected override JsonResult Json (object data,
+              string contentType, Encoding contentEncoding,
+              JsonRequestBehavior behavior)
+        {
+            return new JsonResult ()
+            {
+                Data = data,
+                ContentType = contentType,
+                ContentEncoding = contentEncoding,
+                JsonRequestBehavior = behavior,
+                MaxJsonLength = Int32.MaxValue
+            };
+        }
+        #endregion
+    }
+
+}
---
+++ C# 资源/asp.net/aspnetMVCProject/aspnetMVCProject/Controllers/User/UserController.cs
@@ -0,0 +1,44 @@
+﻿using aspnetMVCProject.Models;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace aspnetMVCProject.Controllers.User
+{
+    public class UserController: Controller
+    {
+        MyLogger logger = new MyLogger ();
+        string connectionString = ConfigurationManager.ConnectionStrings ["DefaultConnection"].ConnectionString;
+
+        // 获取所有用户
+        [HttpGet]
+        public JsonResult GetUsers ()
+        {
+            DbHelper dbHelper = new DbHelper (connectionString);
+            string sql = "select  [Id]  ,[Name] from [dbo].[User]";
+            DataTable dt = dbHelper.ExecuteQuery (sql);
+            List<user> users = new List<user> { };
+            // TODO:
+            foreach (DataRow row in dt.Rows)
+            {
+                logger.Page_Load (new user (row ["Id"].ToString (), row ["Name"].ToString ()), EventArgs.Empty);
+                users.Add (new user (row ["Id"].ToString (), row ["Name"].ToString ()));
+            }
+            //ViewData.Model = users;
+            //return View ("UserIndex");
+            var result = Json (new { users, success = true }, JsonRequestBehavior.AllowGet);
+            return result;// 返回JSON数据
+        }
+
+
+        public ActionResult UserView ()
+        {
+            return View ();
+        }
+    }
+}
